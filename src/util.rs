a5c86ba5ddc1c6a5386ddcf82211bb0b84dc9e2d
UwUssimo Robinson <uwussimo@icloud.com>
2022-01-28 07:51:20 +0500

    path cleaner

--- src/util.rs
+++ src/util.rs
@@ -1,19 +1,28 @@
 use serde_json::Value;
 use std::io::Error;
-use std::path::PathBuf;
 use std::{fs, io};
 
-pub fn read_dir(location: String) -> Result<Vec<PathBuf>, Error> {
+pub fn read_dir(location: String) -> Result<Vec<String>, Error> {
     let inner_location: String = format!("./timetable/{}", location);
-    let entries = fs::read_dir(inner_location)?
-        .map(|res| res.map(|e| e.path()))
+    let clear: String = format!("./timetable/{}", location);
+    let entries = fs::read_dir(&inner_location)?
+        .map(|res| {
+            res.map(|e| {
+                e.path()
+                    .display()
+                    .to_string()
+                    .replace("\\", "/")
+                    .replace(&clear, "")
+                    .replace("/", "")
+            })
+        })
         .collect::<Result<Vec<_>, io::Error>>();
 
     return entries;
 }
 
-pub fn json(locations: Result<Vec<PathBuf>, Error>) -> Value {
-    let text = match locations {
+pub fn json(ctx: Result<Vec<String>, Error>) -> Value {
+    let text = match ctx {
         Ok(content) => serde_json::json!(content),
         Err(error) => serde_json::json!({
             "error": error.to_string(),
