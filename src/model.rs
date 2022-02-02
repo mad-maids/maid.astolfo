2516b4c0a6f280f5cb84886d36bc9bb61f33c660
UwUssimo Robinson <uwussimo@icloud.com>
2022-02-02 06:00:55 +0500

    Update model.rs

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
-pub struct User {
+pub struct Group {
     pub id: String,
     pub module: String,
     pub link: String,
