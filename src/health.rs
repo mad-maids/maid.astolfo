455fcf4d1004988db55517da8435adbdca27d23f
UwUssimo Robinson <uwussimo@icloud.com>
2022-03-14 18:35:43 +0500

    Update health.rs

--- src/health.rs
+++ src/health.rs
@@ -1,11 +1,12 @@
 use log::{error, info, warn};
 use serde::{Deserialize, Serialize};
-use std::error::Error;
-use std::fmt::format;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use std::{fs, io};
+
+static APP_USER_AGENT: &str =
+  concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 fn check(path: &str) -> bool {
   Path::new(path).exists()
@@ -30,16 +31,6 @@ struct Config {
   version: String,
   modules: Vec<String>,
 }
-
-fn module_extract(modules: Vec<Asset>) -> Vec<String> {
-  return modules
-    .iter()
-    .map(|module| module.name.replace(".zip", ""))
-    .collect();
-}
-
-static APP_USER_AGENT: &str =
-  concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 pub async fn health() {
   // Get info about latest builds
@@ -66,19 +57,13 @@ pub async fn health() {
     if parsed.id == target.id {
       info!("Assets are up to date, let's check health of modules");
 
-      let raw = Config {
-        id: target.id,
-        version: target.tag_name,
-        modules: module_extract(target.assets.clone()),
-      };
-
-      let config = serde_json::to_string_pretty(&raw).unwrap();
-      let mut file = File::create("config.json").unwrap();
-      file.write_all(config.as_bytes()).unwrap();
-
-      match check_modules(module_extract(target.assets)).await {
-        Ok(..) => println!("Yes"),
-        Err(e) => println!("{}", e),
+      match check_modules(module_extract(target.assets.clone())).await {
+        Ok(..) => info!("Seems like all modules are alright!"),
+        Err(e) => {
+          error!("{}", e);
+          update(target.assets.clone()).await;
+          save_config(target.clone()).await;
+        }
       }
     }
 
@@ -88,7 +73,8 @@ pub async fn health() {
     }
   } else {
     error!("No config detected!");
-    // update(target.assets).await
+    update(target.assets.clone()).await;
+    save_config(target).await;
   }
 }
 
@@ -104,24 +90,45 @@ async fn check_modules(modules: Vec<String>) -> Result<(), &'static str> {
   Ok(task)
 }
 
-// async fn update(modules: Vec<Asset>) {
-//
-//   for file in target.assets {
-//     let mut dumpfile = tempfile::tempfile().unwrap();
-//
-//     let resp = reqwest::get(file.browser_download_url.to_string())
-//       .await
-//       .expect("Problems with Internet connectivity!")
-//       .bytes()
-//       .await
-//       .expect("Can't convert source into bytes!");
-//     io::copy(&mut resp.as_ref(), &mut dumpfile)
-//       .expect("Failed to copy content");
-//     let mut zip = zip::ZipArchive::new(dumpfile).unwrap();
-//
-//     // Extract files inside folder
-//     zip
-//       .extract(file.name.replace(".zip", ""))
-//       .expect("Couldn't extract...");
-//   }
-// }
+async fn update(modules: Vec<Asset>) {
+  for file in modules {
+    let mut dumpfile = tempfile::tempfile().unwrap();
+
+    let resp = reqwest::get(file.browser_download_url.to_string())
+      .await
+      .expect("Problems with Internet connectivity!")
+      .bytes()
+      .await
+      .expect("Can't convert source into bytes!");
+    io::copy(&mut resp.as_ref(), &mut dumpfile)
+      .expect("Failed to copy content");
+    let mut zip = zip::ZipArchive::new(dumpfile).unwrap();
+
+    // Extract files inside folder
+    zip
+      .extract(file.name.replace(".zip", ""))
+      .expect("Couldn't extract...");
+  }
+}
+
+fn module_extract(modules: Vec<Asset>) -> Vec<String> {
+  return modules
+    .iter()
+    .map(|module| module.name.replace(".zip", ""))
+    .collect();
+}
+
+async fn save_config(config: GitHub) {
+  let raw = Config {
+    id: config.id,
+    version: config.tag_name,
+    modules: module_extract(config.assets),
+  };
+
+  let config = serde_json::to_string_pretty(&raw).unwrap();
+  let mut file =
+    File::create("config.json").expect("Couldn't create JSON file!");
+  file
+    .write_all(config.as_bytes())
+    .expect("Can't write JSON file!");
+}
